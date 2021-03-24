82ac887fbfe23864d930c7282faf7ca45f423af0
JoePeterson51 <>
2021-03-24 14:49:18 -0600

    Joe Peterson

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,28 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name 
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons << name 
+  end 
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase 
+    end
+  end 
+
+  def over_capacity?
+    @patrons.count > @capacity
+  end 
+
+  def kick_out
+    while over_capacity?
+      @patrons.pop
+    end 
+  end 
 end
